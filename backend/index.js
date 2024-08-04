ab0da26fd0cd56d98f738be62f44d2f4a53e07e6
Archita-0T <architatiwari961@gmail.com>
2024-08-04 12:39:24 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
-const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
+const express = require("express");
+const path = require('path');     //deployment
 const app = express();
+
+
 
 const userRoutes = require("./routes/User");
 const paymentRoutes = require("./routes/Payments");
@@ -14,8 +19,7 @@ const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const { cloudnairyconnect } = require("./config/cloudinary");
 
-const dotenv = require("dotenv");
-dotenv.config();
+
 
 const PORT = process.env.PORT || 5000;
 database.connect();
@@ -50,11 +54,30 @@ app.use("/api/v1/course", CourseRoutes);
 
 app.use("/api/v1/contact", require("./routes/ContactUs"));
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to the API",
+
+//--------------------------------------------DEPLOYMENT----------------------------------------------------
+console.log('NODE_ENV:',process.env.NODE_ENV);
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static(path.join(__dirname1,"/frontend/build")));
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  })
+
+}
+else{
+  app.get("/", (req, res) => {
+    res.status(200).json({
+      message: "Welcome to the API",
+    });
   });
-});
+
+}
+
+
+//--------------------------------------------DEPLOYMENT----------------------------------------------------
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
